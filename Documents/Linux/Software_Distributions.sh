768511ade01d947955d0b45d3f1318d596ba4c0f
shubhangi-singh21 <shubhangifas@gmail.com>
2020-06-24 23:12:41 +0530

    content update

--- Documents/Linux/Software_Distributions.sh
+++ Documents/Linux/Software_Distributions.sh
@@ -33,6 +33,27 @@ Maintainer: GitHub <atom@github.com>
 Architecture: amd64
 Version: 1.21.0
 
+#Installing software using apt
+#You can also install programs on Linux, using apt too, which handles dependencies for you to simplify the installation process.
+#Example, we will install the program 7-zip, using apt
+
+sudo apt install p7zip-full
+
+#You can verify the installation using dpkg, with the command:
+
+dpkg -s p7zip-full
+
+#You may see output similar to:
+Package: p7zip-full
+Status: install ok installed
+Priority: optional
+Section: utils
+Insalled-Size: 3866
+Maintainer: Ubuntu Developers <ubuntu-devel-discuss@lists.ubuntu.com>
+Architecture: amd64
+Source: p7zip
+Version: 9.20.1~dfsg.1-4.2
+
 #Extracting an Archive
 
 #From the terminal, change to the directory where yourfile.tar has been downloaded.
@@ -63,6 +84,26 @@ tar: Removing leading '/' from member names
 #Using the ls command in your current directory, you may see:
 Planets.tar
 
+#Uninstalling Software
+#Uninstallation can also be handled by apt by using "remove" instead of "install"as the argument.
+#Example, here we will uninstall GIMP, which is already installed in my machine.
 
+sudo apt remove gimp
 
+#You'll be prompted to confirm your decision midway through the uninstallation process. Confirm this by typing "Y" (for "Yes")
+#Then GIMP will be uninstalled.
+
+#You can verify this using dpkg command:
+
+dpkg -s gimp
+
+#You may see output similar to:
+Package: gimp
+Status: deinstall ok config-files
+Priority: optional
+Section: graphics
+Insalled-Size: 15618
+Maintainer: Ubuntu Developers <ubuntu-devel-discuss@lists.ubuntu.com>
+Architecture: amd64
+Version: 2.8.16-1ubuntu1.1
 

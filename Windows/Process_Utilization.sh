8e1924da9a25a0c1951142ac0e7ecb9ee005e55d
Shuhangi Singh <shubhangifas@gmail.com>
2020-07-17 00:48:32 +0530

    content update

--- Windows/Process_Utilization.sh
+++ Windows/Process_Utilization.sh
@@ -38,11 +38,37 @@ Get-Process -Name "totally_not_malicious"
 Get-Process -Name "*razzle*"
 
 #This will show two processes that contain "razzle" in their name:
+
+Handles   NPM(K)    PM(K)   WS(K)   CPU(s)    Id    SI  ProcessName
+-------   ------    -----   -----   ------    --    --  -----------
+    182       13     2536   10548   614.89   3668    0  my_cat_razzle
+    182       13     2532   10504   632.41   3996    0  razzle_dazzle
+
 You can use taskkill, like before, once for each of the "razzle" processes:
 
 Note: Make sure you replace/substitute the "[PROCESS ID]" with id of the process you got from the previous command.
 
-taskkill /F /PID [PROCESS ID]
+taskkill /F /PID 3668
+
+#You should see this message after running taskkill with the PID for your process, which will likely be different than the ID specified here:
+
+SUCCESS: The process with PID 3668 has been terminated.
+
+taskkill /F /PID 3668
+
+#You should see this message after running taskkill with the PID for your process, which will likely be different than the ID specified here:
+
+SUCCESS: The process with PID 3996 has been terminated.
+
+#You can use Get-Process again to verify that the processes have been ended:
+
+Get-Process -Name "*razzle*"
+
+#You shouldn't see any processes in the output.
+#When you ran this before to verify that the malicious process had been terminated, it printed an error message because the specifically-named process was not present.
+#When you use a wildcard (*) in the search, you aren't looking for an exact match. 
+#So, rather than an error message, the command outputs nothing at all (because there are no matches):
+
 
     
     
